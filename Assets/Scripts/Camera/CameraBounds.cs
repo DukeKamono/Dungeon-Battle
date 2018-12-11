38c611787ce2a2ecdf70d6d13e6ac27262bf5911
James T Moore <dukekamono@hotmail.com>
2018-12-10 23:24:14 -0800

    Test Scenes

---
+++ Assets/Scripts/Camera/CameraBounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBounds : MonoBehaviour
+{
+	private BoxCollider2D cameraBounds;
+	private CameraManager cameraManager;
+
+	// Use this for initialization
+	void Start()
+	{
+		cameraBounds = GetComponent<BoxCollider2D>();
+		cameraManager = FindObjectOfType<CameraManager>();
+		cameraManager.SetBounds(cameraBounds);
+	}
+}
