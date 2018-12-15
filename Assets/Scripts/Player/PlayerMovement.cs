417feeab9a5f3f5d25f93bc488a33c072480c232
James T Moore <dukekamono@hotmail.com>
2018-12-14 17:46:00 -0800

    Basic basic basic movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed;
+	public bool isPlayerMoving;
+	public Vector2 facingDirection;
+
+	private Rigidbody2D currentRigidbody2D;
+	private Vector2 vector2Input;
+
+	// Use this for initialization
+	void Start ()
+	{
+		currentRigidbody2D = GetComponent<Rigidbody2D>();
+
+		isPlayerMoving = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Don't move me if I can't move.
+		if (!isPlayerMoving)
+		{
+			currentRigidbody2D.velocity = Vector2.zero;
+		}
+		else
+		{
+			vector2Input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+			//If an input was pressed.
+			if (vector2Input != Vector2.zero) {
+				currentRigidbody2D.velocity = new Vector2 (vector2Input.x * speed, vector2Input.y * speed);
+				facingDirection = vector2Input;
+			}
+			else
+			{
+				currentRigidbody2D.velocity = Vector2.zero;
+			}
+		}
+	}
+}
